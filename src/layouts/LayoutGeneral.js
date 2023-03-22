c08da3ee9f1427f5a75dceae9e5a3c5b808cad7d
LuisaB <lbetancur72@gmail.com>
2023-03-21 21:02:59 -0500

    prueba2

--- src/layouts/LayoutGeneral.js
+++ src/layouts/LayoutGeneral.js
@@ -1,8 +1,6 @@
 import React, { useState } from 'react'
 import { Layout } from 'antd'
 import { MenuSider } from '../components/MenuSider/MenuSider';
-
-
 export const LayoutGeneral = (props) => {
     const {children}=props;
     const [menuCollapsed, setMenuCollapsed]= useState(false);
